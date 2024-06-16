554bb1b3f4b379e2236c24b03623da92423f3384
platane <me@platane.me>
2024-06-16 16:02:41 +0200

    ✨

--- add-cotation.ts
+++ add-cotation.ts
@@ -1,4 +1,4 @@
-const file = Bun.file("./flat2.svg");
+const file = Bun.file("./flat.svg");
 
 const text = await file.text();
 
@@ -47,60 +47,82 @@ for (const polygon of polygons) {
 //
 // add bounding boxes
 
-const margin = 10;
-{
-  const clusters = polygons.map((p) => [p]);
+const clusters = polygons.map((p) => [p]);
 
-  for (let i = clusters.length; i--; ) {
-    const keys = clusters[i].flat().map(([x, y]) => x + "-" + y);
+for (let i = clusters.length; i--; ) {
+  const keys = clusters[i].flat().map(([x, y]) => x + "-" + y);
 
-    for (let j = i; j--; ) {
-      if (
-        clusters[j]
-          .flat()
-          .map(([x, y]) => x + "-" + y)
-          .some((k) => keys.includes(k))
-      ) {
-        clusters[j].push(...clusters.splice(i, 1)[0]);
-        break;
-      }
+  for (let j = i; j--; ) {
+    if (
+      clusters[j]
+        .flat()
+        .map(([x, y]) => x + "-" + y)
+        .some((k) => keys.includes(k))
+    ) {
+      clusters[j].push(...clusters.splice(i, 1)[0]);
+      break;
     }
-  }
-
-  for (const cluster of clusters) {
-    const points = cluster.flat();
-
-    let minX = Infinity;
-    let minY = Infinity;
-    let maxX = -Infinity;
-    let maxY = -Infinity;
-
-    for (const [x, y] of points) {
-      minX = Math.min(minX, x - margin);
-      minY = Math.min(minY, y - margin);
-      maxX = Math.max(maxX, x + margin);
-      maxY = Math.max(maxY, y + margin);
-    }
-
-    edges.set(minX + "" + minY + minX + "" + maxY, {
-      a: [minX, minY],
-      b: [minX, maxY],
-    });
-    edges.set(minX + "" + minY + maxX + "" + minY, {
-      a: [minX, minY],
-      b: [maxX, minY],
-    });
-
-    annotations +=
-      "\n" +
-      `<rect x="${minX}" y="${minY}" width="${maxX - minX}" height="${
-        maxY - minY
-      }" stroke="#666" stroke-width="1" fill="none" />`;
   }
 }
 
-const font = "42px monospace";
-const scale = 0.1;
+for (const cluster of clusters) {
+  const points = cluster.flat();
+
+  const margin = 10;
+
+  let minX = Infinity;
+  let minY = Infinity;
+  let maxX = -Infinity;
+  let maxY = -Infinity;
+
+  for (const [x, y] of points) {
+    minX = Math.min(minX, x - margin);
+    minY = Math.min(minY, y - margin);
+    maxX = Math.max(maxX, x + margin);
+    maxY = Math.max(maxY, y + margin);
+  }
+
+  edges.set(minX + "" + minY + minX + "" + maxY, {
+    a: [minX, minY],
+    b: [minX, maxY],
+  });
+  edges.set(minX + "" + minY + maxX + "" + minY, {
+    a: [minX, minY],
+    b: [maxX, minY],
+  });
+
+  annotations +=
+    "\n" +
+    `<rect x="${minX}" y="${minY}" width="${maxX - minX}" height="${
+      maxY - minY
+    }" stroke="#666" stroke-width="1" fill="none" />`;
+}
+
+for (let i = clusters.length; i--; ) {
+  for (let j = clusters[i].length; j--; ) {
+    let cx = 0;
+    let cy = 0;
+    for (const [x, y] of clusters[i][j]) {
+      cx += x;
+      cy += y;
+    }
+    cx /= clusters[i][j].length;
+    cy /= clusters[i][j].length;
+
+    annotations +=
+      "\n" +
+      '<text class="label" ' +
+      `x="${cx - 30}" ` +
+      `y="${cy - 10}" ` +
+      ">" +
+      (i + 10).toString(36).toUpperCase() +
+      (j + 1) +
+      "</text>";
+  }
+}
+
+const font = "54px monospace";
+const scale = 0.095;
 
 for (const { a, b } of edges.values()) {
   const cx = (a[0] + b[0]) / 2;
@@ -113,14 +135,18 @@ for (const { a, b } of edges.values()) {
 
   annotations +=
     "\n" +
-    `<line x1="${cx - 13}" x2="${
-      cx + 13
-    }" y1="${cy}" y2="${cy}" stroke="#555" stroke-width="1.5" />` +
+    `<line ` +
+    `x1="${cx - 20}" ` +
+    `x2="${cx + 20}" ` +
+    `y1="${cy}" ` +
+    `y2="${cy}" ` +
+    `stroke="#555" stroke-width="1.5" />` +
+    "\n" +
     `<circle cx="${cx}" cy="${cy}" r="3" fill="#555" />` +
     "\n" +
-    "<text " +
+    '<text class="quote" ' +
     `x="${cx - textLength / 2}" ` +
-    `y="${cy - 3}"` +
+    `y="${cy - 3}" ` +
     ">" +
     label +
     "</text>";
@@ -132,12 +158,20 @@ annotations += `\n<style>
         text-shadow: 1px 0px 2px white, -1px 0px 2px white;
         letter-spacing:-0.1em;
     }
+    
+    text.label{
+        font-size: 80px;
+        fill: #1122ee88;
+        font-weight: bold;
+    }
 </style>
 `;
 
 await Bun.write(
-  "./flat2-annotated.svg",
-  text.replace(/<\/svg>/, (s) => "<g>" + annotations + "</g>" + s)
+  "./flat-annotated.svg",
+  text
+    .replace(/<\/svg>/, (s) => "<g>" + annotations + "</g>" + s)
+    .replace(/<svg /, (s) => s + ' style="width:100%;height:100%" ')
 );
 
 console.log("generated");
